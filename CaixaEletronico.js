26e2a1f96cd502e38e87c5723fe609ff85d0461d
David Marins <davidmarinsdiogo01@gmail.com>
2022-09-01 19:11:10 -0300

    Sistema de SAQUE ou DEPÒSITO do caixa Eletronico

--- CaixaEletronico.js
+++ CaixaEletronico.js
@@ -1,5 +1,31 @@
 
+var contador = true
+var saldo = 1000
+var nome = prompt("Digite seu nomê :")
+var cpf = parseInt(prompt("Digite seu CPF :"))
+console.log("Nome Cliente :", nome, "\nCPF :", cpf);
 
-var nome = prompt ("Digite seu nomê :")
-var cpf = parseInt (prompt("Digite seu CPF :"))
-console.log("Nome Cliente :",nome,"\nCPF :",cpf);
+
+while (contador) {
+  
+
+        var opcao = prompt("Digite:(1) SAQUE ou (2) DEPOSITO")
+
+        var valor = parseFloat(prompt("Digite um valor: "))
+        console.log("Valor digitado R$",valor)
+
+        if (opcao == 1) {
+            saldo = saldo - valor
+
+        }
+        if (opcao == 2) {
+
+            saldo = saldo + valor
+        }
+        console.log("Seu saldo em conta é R$", saldo)
+
+    var continuar = prompt("deseja continur?\n Aperte (ENTER), se não (3) ")
+    if (continuar == 3) {
+        contador = false
+    }
+}
